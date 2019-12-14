9ceb7d8129c3809b468d83bec76b35a4744d67b5
pycodeDev <anangfriendshell@gmail.com>
2019-12-14 16:46:38 +0700

    add menu and fix img admin

--- application/views/admin/content/dashboard.php
+++ application/views/admin/content/dashboard.php
@@ -1,38 +1,38 @@
-<div class="row">
-    <div class="col-sm-12">
-        <div class="card">
-            <div class="card-header">
-                <h5>Dashboard</h5>
-                <span>lorem ipsum dolor sit amet, consectetur adipisicing elit</span>
-                <div class="card-header-right">
-                    <ul class="list-unstyled card-option">
-                        <li>
-                            <i class="fa fa fa-wrench open-card-option"></i>
-                        </li>
-                        <li>
-                            <i class="fa fa-window-maximize full-card"></i>
-                        </li>
-                        <li>
-                            <i class="fa fa-minus minimize-card"></i>
-                        </li>
-                        <li>
-                            <i class="fa fa-refresh reload-card"></i>
-                        </li>
-                        <li>
-                            <i class="fa fa-trash close-card"></i>
-                        </li>
-                    </ul>
-                </div>
-            </div>
-            <div class="card-block">
-                <p>
-                    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
-                    laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
-                    in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est
-                    laborum."
-                </p>
-            </div>
-        </div>
-    </div>
-
-</div>
+                                            <!-- Project statustic start -->
+                                            <div class="col-xl-12">
+                                                <div class="card proj-progress-card">
+                                                    <div class="card-block">
+                                                        <div class="row">
+                                                            <div class="col-xl-3 col-md-6">
+                                                                <h6>Published Project</h6>
+                                                                <h5 class="m-b-30 f-w-700">532<span class="text-c-green m-l-10">+1.69%</span></h5>
+                                                                <div class="progress">
+                                                                    <div class="progress-bar bg-c-red" style="width:25%"></div>
+                                                                </div>
+                                                            </div>
+                                                            <div class="col-xl-3 col-md-6">
+                                                                <h6>Completed Task</h6>
+                                                                <h5 class="m-b-30 f-w-700">4,569<span class="text-c-red m-l-10">-0.5%</span></h5>
+                                                                <div class="progress">
+                                                                    <div class="progress-bar bg-c-blue" style="width:65%"></div>
+                                                                </div>
+                                                            </div>
+                                                            <div class="col-xl-3 col-md-6">
+                                                                <h6>Successfull Task</h6>
+                                                                <h5 class="m-b-30 f-w-700">89%<span class="text-c-green m-l-10">+0.99%</span></h5>
+                                                                <div class="progress">
+                                                                    <div class="progress-bar bg-c-green" style="width:85%"></div>
+                                                                </div>
+                                                            </div>
+                                                            <div class="col-xl-3 col-md-6">
+                                                                <h6>Ongoing Project</h6>
+                                                                <h5 class="m-b-30 f-w-700">365<span class="text-c-green m-l-10">+0.35%</span></h5>
+                                                                <div class="progress">
+                                                                    <div class="progress-bar bg-c-yellow" style="width:45%"></div>
+                                                                </div>
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            <!-- Project statustic end -->
